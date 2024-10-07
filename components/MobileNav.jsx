918013863b7a1a152d03c2e5626e4e3ac83d458b
inga-divra <ingady@gmail.com>
2024-10-07 17:03:15 +0300

    Updated MobileNav: integrated Sheet components for mobile menu, used usePathname for path tracking, and added CiMenuFries icon (react-icons) as menu trigger. Added new class to IoMdClose icon in Sheet for styling.

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -5,13 +5,40 @@ import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet"
 import Link from "next/link"
 import { CiMenuFries } from "react-icons/ci";
 
+const links = [
+    {
+        name: 'home',
+        path: '/',
+    },
+    {
+        name: 'services',
+        path: '/services',
+    },
+    {
+        name: 'resume',
+        path: '/resume',
+    },
+    {
+        name: 'work',
+        path: '/work',
+    },
+    {
+        name: 'contact',
+        path: '/contact',
+    },
+]
 
 const MobileNav = () => {
-    return (
-        <div>
-            MobileNav
-        </div>
-    )
+    const pathname = usePathname();
+    return <Sheet>
+        <SheetTrigger className='flex justify-center items-center'>
+            <CiMenuFries className='text-[32px] text-accent' />
+        </SheetTrigger>
+        <SheetContent className='flex flex-col'>
+            {/* Logo */}
+            <div>Logo</div>
+        </SheetContent>
+    </Sheet>
 }
 
 export default MobileNav
