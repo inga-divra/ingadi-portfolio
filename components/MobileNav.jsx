205d021524643c447d4d69f35593365e6957ce46
inga-divra <ingady@gmail.com>
2024-10-07 18:08:02 +0300

    Set up mobile menu navigation with logo, menu items, and appropriate classes for styling and responsiveness in MobileNav component.

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -36,7 +36,26 @@ const MobileNav = () => {
         </SheetTrigger>
         <SheetContent className='flex flex-col'>
             {/* Logo */}
-            <div>Logo</div>
+            <div className='mt-32 mb-40 text-center text-2xl'>
+                <Link href='/'>
+                    <h1 className='text-4xl font-semibold'>
+                        IngaDI<span className='text-accent'>.</span>
+                    </h1>
+                </Link>
+            </div>
+            {/* nav */}
+            <nav className='flex flex-col justify-center items-center gap-8'>
+                {links.map((link, index) => {
+                    return <Link
+                        className={`${link.path === pathname &&
+                            'text-accent border-b-2 border-accent'
+                            } text-xl capitalize hover:text-accent transition-all`}
+                        href={link.path}
+                        key={index}>
+                        {link.name}
+                    </Link>
+                })}
+            </nav>
         </SheetContent>
     </Sheet>
 }
