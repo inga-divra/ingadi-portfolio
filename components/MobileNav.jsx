549bb36611038ed9ccecc38cab3577222cb7fe1e
inga-divra <ingady@gmail.com>
2024-10-07 12:33:09 +0300

    Updated MobileNav component: applied 'use client' for client-side rendering, integrated Sheet, SheetContent, and SheetTrigger for mobile menu functionality, and imported usePathname for dynamic path tracking. Installed react-icons library and added CiMenuFries icon as the menu trigger.

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,3 +1,11 @@
+'use client'
+
+import { usePathname } from "next/navigation"
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet"
+import Link from "next/link"
+import { CiMenuFries } from "react-icons/ci";
+
+
 const MobileNav = () => {
     return (
         <div>
