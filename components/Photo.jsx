cf65b664dc16364944fa38ff3d25efd1d3a7974f
inga-divra <ingady@gmail.com>
2024-10-08 18:41:15 +0300

    fixed hero img position

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -20,7 +20,7 @@ const Photo = () => {
                         opacity: 1,
                         transition: { delay: 2.4, duration: 0.4, ease: 'easeInOut' },
                     }}
-                    className='w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute'
+                    className='w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten'
                 >
                     <Image
                         src='/assets/hero5.svg'
