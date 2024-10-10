0c1fdebf98791eb56b6d29f8e7a9dc779c4ded0f
inga-divra <ingady@gmail.com>
2024-10-10 12:33:14 +0300

    updated scroll-area ui classes

--- components/ui/scroll-area.jsx
+++ components/ui/scroll-area.jsx
@@ -1,4 +1,4 @@
-"use client";
+"Use client";
 
 import * as React from "react";
 import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area";
@@ -12,7 +12,9 @@ const ScrollArea = React.forwardRef(
       className={cn("relative overflow-hidden", className)}
       {...props}
     >
-      <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
+      <ScrollAreaPrimitive.Viewport
+        className={cn("h-full w-full overflow-y-auto rounded-[inherit]")}
+      >
         {children}
       </ScrollAreaPrimitive.Viewport>
       <ScrollBar />
