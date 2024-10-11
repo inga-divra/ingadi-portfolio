e52e1af8adbf5a13a57398f8a332c6611c316f52
inga-divra <ingady@gmail.com>
2024-10-11 12:48:00 +0300

    Added necessary imports and created a personal info array into the Contact page. Updated the info array with placeholders for phone, email, and address, along with relevant icons (FaPhoneAlt, FaEnvelope, FaMapMarkerAlt).

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,9 +1,46 @@
+'use client';
+
+import React from 'react';
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa';
+import { motion } from 'framer-motion';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui/select';
+
+const info = [
+    {
+        icon: <FaPhoneAlt />,
+        title: 'Phone',
+        description: '(+358) 44 959 7074',
+    },
+    {
+        icon: <FaEnvelope />,
+        title: 'Email',
+        description: 'inga.divra@gmail.com',
+    },
+    {
+        icon: <FaMapMarkerAlt />,
+        title: 'Address',
+        description: 'Helsinki, Finland',
+    },
+];
+
 const Contact = () => {
     return (
         <div>
             Contact page
         </div>
-    )
-}
+    );
+};
 
-export default Contact
+export default Contact;
