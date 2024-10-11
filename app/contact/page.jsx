8b4aaabf0e8bff9bfa67e49d516c2ea024bb369c
inga-divra <ingady@gmail.com>
2024-10-11 14:07:07 +0300

    Added form layout with necessary styling, including input fields and a description encouraging collaboration. Updated input fields to be required for validation. Also, adjusted classes in the input UI component for consistent styling and appearance.

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -37,9 +37,37 @@ const info = [
 
 const Contact = () => {
     return (
-        <div>
-            Contact page
-        </div>
+        <motion.div className='py-6' initial={{ opacity: 0 }}
+            animate={{
+                opacity: 1,
+                transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+            }}>
+            <div className='container mx-auto'>
+                <div className='flex flex-col xl:flex-row gap-[30px]'>
+                    {/* form */}
+                    <div className='xl:w-[54%] order-2 xl:order-none'>
+                        <form
+                            className='flex flex-col gap-6 p-10 bg-[#2A2A33] rounded-xl'>
+                            <h3 className='text-4xl text-accent'>Let's work together</h3>
+                            <p className='text-white/80'>
+                                I'm excited to collaborate with you on your next project. Fill out the form below, and let's start building something amazing together.
+                            </p>
+                            {/* input fields */}
+                            <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+                                <Input type='text' placeholder='First Name' required />
+                                <Input type='text' placeholder='Last Name' required />
+                                <Input type='email' placeholder='Email Address' required />
+                                <Input type='tel' placeholder='Phone Number' required />
+                            </div>
+                        </form>
+                    </div>
+                    {/* info */}
+                    <div>
+                        <div className='flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0'>info</div>
+                    </div>
+                </div>
+            </div>
+        </motion.div>
     );
 };
 
