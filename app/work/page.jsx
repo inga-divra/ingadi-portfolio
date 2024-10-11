68dfc6ebb888ca83219074d0ab73eee537789c0d
inga-divra <ingady@gmail.com>
2024-10-11 09:59:59 +0300

    Rendered project info content on the Work page, displaying project details such as number, category, description, stack, and action buttons with tooltips for live project and GitHub links.

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,33 +1,37 @@
 'use client'
 
-import { motion } from "framer-motion";
-import React, { useState } from "react";
+import React, { useState } from 'react'
+import { motion } from 'framer-motion'
 
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
 
-import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+import { BsArrowUpRight, BsGithub } from 'react-icons/bs'
 
-import Link from "next/link";
-import Image from "next/image";
+import Link from 'next/link'
+import Image from 'next/image'
+
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
+
 
 const projects = [
     {
         num: '01',
-        category: 'frontend',
+        category: 'fullstack',
         title: 'E-Commerce App',
         description:
             'A responsive e-commerce platform built with Next.js and TypeScript, offering a smooth shopping experience and dynamic product listings.',
         stack: [
             { name: 'Next.js' },
             { name: 'TypeScript' },
-            { name: 'Tailwind CSS' },
+            { name: 'Tailwind' },
+            { name: 'Prisma' },
+            { name: 'Database' }
         ],
         image: '/assets/work/thumb1.png',
-        live: 'https://ingady-ecommerce.com',
+        live: '#',
         github: 'https://github.com/inga-divra/nextjs-store-project',
     },
-
     {
         num: '02',
         category: 'fullstack',
@@ -78,8 +82,80 @@ const projects = [
 const Work = () => {
     const [project, setProject] = useState(projects[0])
     return (
-        <motion.div>
-            <div className='container mx-auto'>works</div>
+        <motion.div
+            className='min-h-[80vh] flex flex-col justify-center py-12 xl:px-0'
+            initial={{ opacity: 0 }}
+            animate={{
+                opacity: 1,
+                transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+            }}>
+            <div className='container mx-auto'>
+                <div className='flex flex-col xl:flex-row xl:gap-[30px]'>
+                    {/* PROJECT INFO */}
+                    <div className='w-full xl:w-[50%] xl:min-h-[460px] flex-grow flex flex-col xl:justify-between'>
+                        <div className='flex flex-col gap-[30px] h-[50%]'>
+                            {/* outline num */}
+                            <div className='text-8xl leading-none font-extrabold text-transparent text-outline'>
+                                {project.num}
+                            </div>
+                            {/* project category */}
+                            <h2 className='text-[42px] font-bold leading-none group-hover:text-accent 
+                        transition-all duration-500 capitalize'>
+                                {project.category} project
+                            </h2>
+                            {/* project description */}
+                            <p className='text-white/80'>
+                                {project.description}
+                            </p>
+                            {/* stack */}
+                            <ul className='flex gap-4'>
+                                {project.stack.map((item, index) => {
+                                    return <li
+                                        className='text-xl text-accent'
+                                        key={index}>
+                                        {item.name}
+                                        {/* remove the last comma */}
+                                        {index !== project.stack.length - 1 && ','}
+                                    </li>
+                                })}
+                            </ul>
+                            {/* border */}
+                            <div className='border border-white/20'></div>
+                            {/* buttons */}
+                            <div className='flex items-center gap-4'>
+                                {/* Live project btn */}
+                                <Link href={project.live} target='_blank' rel='noopener noreferrer'  >
+                                    <TooltipProvider delayDuration={100}>
+                                        <Tooltip>
+                                            <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                                                <BsArrowUpRight className="text-3xl group-hover:text-accent" />
+                                            </TooltipTrigger>
+                                            <TooltipContent className='bg-[#E7DAF5] text-[#2A2A33] p-2 rounded-md shadow-md'>
+                                                <p>Live project</p>
+                                            </TooltipContent>
+                                        </Tooltip>
+                                    </TooltipProvider>
+                                </Link>
+                                {/* Github project btn */}
+                                <Link href={project.github} target='_blank' rel='noopener noreferrer'  >
+                                    <TooltipProvider delayDuration={100}>
+                                        <Tooltip>
+                                            <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                                                <BsGithub className="text-3xl group-hover:text-accent" />
+                                            </TooltipTrigger>
+                                            <TooltipContent className='bg-[#E7DAF5] text-[#2A2A33] p-2 rounded-md shadow-md'>
+                                                <p>Github repository</p>
+                                            </TooltipContent>
+                                        </Tooltip>
+                                    </TooltipProvider>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                    {/* SLIDER */}
+                    <div className='w-full xl:w-[50%]'>slider</div>
+                </div>
+            </div>
         </motion.div>
     )
 }
