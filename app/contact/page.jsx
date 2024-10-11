d85460714719cbe6bc21cebb469bac2a1c1359b5
inga-divra <ingady@gmail.com>
2024-10-11 15:33:41 +0300

    updated motion section classes

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -8,7 +8,6 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 
-
 const info = [
     {
         icon: <FaPhoneAlt />,
@@ -29,46 +28,64 @@ const info = [
 
 const Contact = () => {
     return (
-        <motion.div className='py-6' initial={{ opacity: 0 }}
+        <motion.section
+            className='flex justify-center items-center min-h-screen'
+            initial={{ opacity: 0 }}
             animate={{
                 opacity: 1,
-                transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
-            }}>
-            <div className='container mx-auto'>
-                <div className='flex flex-col xl:flex-row gap-[30px]'>
-                    {/* form */}
-                    <div className='xl:w-[54%] order-2 xl:order-none'>
-                        <form
-                            className='flex flex-col gap-6 p-10 bg-[#2A2A33] rounded-xl'>
-                            <h3 className='text-4xl text-accent'>Let's work together</h3>
-                            <p className='text-white/80'>
-                                text
-                            </p>
-                            {/* input fields */}
-                            <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
-                                <Input type='text' placeholder='First Name' required />
-                                <Input type='text' placeholder='Last Name' required />
-                                <Input type='email' placeholder='Email Address' required />
-                                <Input type='tel' placeholder='Phone Number' required />
-                            </div>
-                            {/* textarea */}
-                            <Textarea
-                                className='h-[200px]'
-                                placeholder='Type your message here'
-                            />
-                            {/* btn */}
-                            <Button size='md' className='max-w-40'>
-                                Send message
-                            </Button>
-                        </form>
-                    </div>
-                    {/* info */}
-                    <div>
-                        <div className='flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0'>info</div>
-                    </div>
+                transition: { delay: 2.4, duration: 0.4, ease: 'easeIn' },
+            }}
+        >
+            <div className='container mx-auto flex flex-col xl:flex-row items-center gap-[30px]'>
+                {/* Form */}
+                <div className='xl:w-[54%] order-2 xl:order-1'>
+                    <form className='flex flex-col gap-6 p-10 bg-[#2A2A33] rounded-xl'>
+                        <h3 className='text-4xl text-accent'>
+                            Let&apos;s work together
+                        </h3>
+                        <p className='text-white/80'>
+                            I&apos;m excited to collaborate with you on your next project. Fill out the form below, and let&apos;s start building something amazing together.
+                        </p>
+                        {/* Input fields */}
+                        <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+                            <Input type='text' placeholder='First Name' required />
+                            <Input type='text' placeholder='Last Name' required />
+                            <Input type='email' placeholder='Email Address' required />
+                            <Input type='tel' placeholder='Phone Number' required />
+                        </div>
+                        {/* Textarea */}
+                        <Textarea
+                            className='h-[200px]'
+                            placeholder='Type your message here'
+                            required
+                        />
+                        {/* Button */}
+                        <Button size='md' className='max-w-40'>
+                            Send message
+                        </Button>
+                    </form>
+                </div>
+
+                {/* Info */}
+                <div className='flex-1 flex items-center xl:justify-end order-1 xl:order-2 mb-8 xl:mb-0'>
+                    <ul className='flex flex-col gap-10'>
+                        {info.map((item, index) => {
+                            return (
+                                <li key={index} className='flex items-center gap-6'>
+                                    <div className='w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#2A2A33] text-accent rounded-md flex items-center justify-center'>
+                                        <div className='text-[28px]'>{item.icon}</div>
+                                    </div>
+                                    <div className='flex-1'>
+                                        <p className='text-white/80'>{item.title}</p>
+                                        <h3 className='text-xl'>{item.description}</h3>
+                                    </div>
+                                </li>
+                            );
+                        })}
+                    </ul>
                 </div>
             </div>
-        </motion.div>
+        </motion.section>
     );
 };
 
